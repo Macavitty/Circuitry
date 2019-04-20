0fbff30b91ba7cb2f6092150c1d1a7226d4a3b15
prilutskaya <prilutskaya_@ukr.net>
2019-04-20 18:20:50 +0300

    almost completed, _state_ is not used properly yet

--- lab_2/lab2.srcs/sources_1/new/function.v
+++ lab_2/lab2.srcs/sources_1/new/function.v
@@ -1,21 +1,29 @@
 module func(
-    input [7:0] x_bi,
+    input signed [7:0] x_bi,
     input clk, 
     input rst,
-    output [7:0] y_o
+    output reg signed [7:0] y_bo
 );
     parameter signed [7:0] NEG_QUATER   = 8'b1010_0000;
     parameter signed [7:0] THIRD        = 8'b0010_1010;
-
-
-    reg signed [7:0] a_mult, b_mult, y_mult;
-    reg signed [7:0] a_sum, b_sum, y_sum;
-    reg signed [7:0] mux_counst_in,  
-    mux_counst_adr,   
+    parameter signed [7:0] HALF         = 8'b0100_0000;
     
+    localparam IDLE = 1'b0;
+    localparam WORK = 1'b1;
+
+    reg signed [7:0]  y_mult;
+    wire signed [7:0] a_mult, b_mult;
     reg start_mult, busy_mult;
     
+    reg signed [7:0] mux_const_out, mux_x_out;  
+    wire signed [7:0] mux_const_in, mux_x_in;
+    reg [1:0] mux_const_adr;
+    reg mux_x_adr;
+    
+    reg signed [7:0] sum;
     reg [2:0] ctr; 
+    
+    reg state;
 
     mult mult_1 (
         .clk_i(clk),
@@ -29,48 +37,67 @@ module func(
         .y_bo(y_mult)
      );
 
-    sum s(
-        .a_bi(a_sum),
-        .b_bi(b_sum),
-        .y_bo(y_sum)
-    );
     
     mux_41 mux_const(
-        .a(),
-        .x0(),
+        .a(mux_const_adr),
+        .x0(mux_const_in),
         .x1(NEG_QUATER),
         .x2(THIRD),
-        .ans()
+        .ans(mux_const_out) // TODO set as in in mux_x
     );
     
     mux_21 mux_x(
-        .a(),
-        .x0(),
-        .x1(),
-        .ans()
+        .a(mux_x_adr),
+        .x0(x_bi),
+        .x1(mux_x_in),
+        .ans(mux_x_out)
     );
+    
+    assign a_mult = mux_x_out;
+    assign b_mult = mux_const_out;
+    assign mux_const_in = mux_x_out;
+    assign mux_x_in = y_mult;
     
     always@(posedge clk, posedge rst) 
         if (rst) begin
-            a_mult <= 0;
-            b_mult <= 0;
+//            a_mult <= 0;
+//            b_mult <= 0;
             y_mult <= 0;
-            a_sum <= 0;
-            b_sum <= 0;
-            y_sum <= 0;
             ctr <= 0;
+            state <= IDLE;
         end else begin 
-            if (!busy) begin
+            if (!busy_mult) begin
                 case (ctr)
-                    3'd0:
-                    3'd1:
-                    3'd2:
-                    3'd3:
-                    3'd4:
-                endcase    
+                    3'd0: begin
+                            mux_x_adr <= 0;
+                            mux_const_adr <= 0; 
+                            start_mult = 1;                         
+                          end
+                    3'd1: begin 
+                             mux_x_adr <= 1;
+                             mux_const_adr <= 1;
+                             start_mult = 1;
+                           end
+                    3'd2: begin  
+                             mux_x_adr <= 1;
+                             mux_const_adr <= 0;
+                             sum = HALF + y_mult;
+                             start_mult = 1;
+                          end
+                    3'd3: begin 
+                              mux_x_adr <= 1;
+                              mux_const_adr <= 2;
+                              start_mult = 1;
+                          end
+                    3'd4: begin 
+                            sum = sum + y_mult;
+                            y_bo = sum;   
+                            start_mult = 0;
+                          end
+                endcase 
+                // here were a and b mult   
                 ctr <= ctr + 1;
             end
-    
-        end
+         end
 
 endmodule
